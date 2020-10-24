e55e0b7356f5700dfac70d7a57c9c44599f73332
lsauro <t3rrific00@gmail.com>
2020-10-24 14:00:10 +0800

    add about page

---
+++ js/bridge.js
@@ -0,0 +1,32 @@
+var counter = 1;
+
+$(document).ready(function () {
+  $(".login-form").hide();
+  $(".login").css("background", "none");
+
+  $(".login").click(function () {
+    $(".signup-form").hide();
+    $(".login-form").show();
+    $(".signup").css("background", "none");
+    $(".login").css("background", "#2f575d");
+  });
+
+  $(".signup").click(function () {
+    $(".signup-form").show();
+    $(".login-form").hide();
+    $(".login").css("background", "none");
+    $(".signup").css("background", "#2f575d");
+  });
+
+  $(".btn").click(function () {
+    $(".input").val("");
+  });
+
+  setInterval(function () {
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if (counter > 3) {
+      counter = 1;
+    }
+  }, 7000);
+});
