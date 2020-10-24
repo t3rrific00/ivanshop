cf7da04b9ee5fab2098110a29fd83c5f0c9494a5
lsauro <t3rrific00@gmail.com>
2020-10-25 07:03:58 +0800

    add back to top

--- js/bridge.js
+++ js/bridge.js
@@ -1,9 +1,15 @@
 var counter = 1;
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
 $(document).ready(function () {
 
-  $(".login-form").hide();
-  $(".login").css("background", "none");
+  // $(".signup-form").hide();
+  // $(".signup").css("background", "none");
 
   $(".login").click(function () {
     $(".signup-form").hide();
@@ -23,11 +29,26 @@ $(document).ready(function () {
     $(".input").val("");
   });
 
-  setInterval(function () {
-    document.getElementById("radio" + counter).checked = true;
-    counter++;
-    if (counter > 3) {
-      counter = 1;
-    }
-  }, 7000);
 });
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 7000);
