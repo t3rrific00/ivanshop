e6a0fc0bdf051f898be1915992734f4c07bfcd45
lsauro <t3rrific00@gmail.com>
2020-10-25 09:00:00 +0800

    add contact us

--- js/bridge.js
+++ js/bridge.js
@@ -43,7 +43,12 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
+
+// When the user clicks on the button, scroll to the bottom of the document
+function bottomFunction() {
+  window.scrollTo(0,document.body.scrollHeight);
+}
 
 setInterval(function () {
   document.getElementById("radio" + counter).checked = true;
